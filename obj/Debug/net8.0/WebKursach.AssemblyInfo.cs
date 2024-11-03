2231eeace4d8c7bfadb53e82181ba4f9b5f41703
unknown <you@example.com>
2024-11-03 12:47:21 +0300

    gfh

--- obj/Debug/net8.0/WebKursach.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebKursach.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebKursach")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+468b527c75d8c9d2c0d5860652a2963e661c1ae2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8520134e9ffa2e51edd64f7e294d06d83b6cada0")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebKursach")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebKursach")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
